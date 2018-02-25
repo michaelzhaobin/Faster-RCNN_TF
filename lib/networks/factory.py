efbfd58f61fe2f64704b47375e69c9d054ba515c
michaelzhaobin <31604328+michaelzhaobin@users.noreply.github.com>
2018-02-25 20:10:43 +0800

    Update factory.py

--- lib/networks/factory.py
+++ lib/networks/factory.py
@@ -19,11 +19,12 @@ import tensorflow as tf
 #__sets['VGGnet_test'] = networks.VGGnet_test()
 
 
+#name: VGGnet_train
 def get_network(name):
     """Get a network by name."""
     #if not __sets.has_key(name):
     #    raise KeyError('Unknown dataset: {}'.format(name))
-    #return __sets[name]
+    #return __sets[name
     if name.split('_')[1] == 'test':
        return networks.VGGnet_test()
     elif name.split('_')[1] == 'train':
