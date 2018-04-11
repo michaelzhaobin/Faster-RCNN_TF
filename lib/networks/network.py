5cd5c5ae967892f57b29476baab1d9467b532196
michaelzhaobin <31604328+michaelzhaobin@users.noreply.github.com>
2018-04-11 16:00:01 +0800

    Update network.py

--- lib/networks/network.py
+++ lib/networks/network.py
@@ -233,12 +233,12 @@ blob[:,1:5]: x1,y1,x2,y2(in the original image)
            
             return rois, labels, bbox_targets, bbox_inside_weights, bbox_outside_weights
 """
-(1) rois: (num of finally left proposal（128）, 5) blob[:,0]=0; blob[:-2,1:5] = x1,y1,x2,y2(pred box); blob[-2:,1:5] = x1,y1,x2,y2(gt_box)
-          [0:fg_rois_per_this_image（32）]: the left foregound; [fg_rois_per_this_image（32）:]:the left background
-(2) the final classes of the ground truth correspounding to per pred box [num of finally left proposal（128）,1] 
-        for ex: [[9],[15],[15],[15],[9],[9]....]
-
-(3): num of finally left proposals * 4*21: [dx,dy,dw,dh] of 1 class in 21
+(1) rois: (num of finally left proposal（根据每个roi和gtbox的overlaps大小来确定留下的fg和bg，多了的随机选择) blob[:,0]=0; 
+          blob[:-2,1:5] = x1,y1,x2,y2(pred box in original image); blob[-2:,1:5] = x1,y1,x2,y2(gt_box)
+          [0:fg_rois_per_this_image（大约42）]: the left foregound; [fg_rois_per_this_image（128-42）:]:the left background
+(2) labels: the final classes of the ground truth correspounding to per pred box [num of finally left proposal（128）,] 
+        for ex: [9,15,15,15,9,9....]
+(3): num of finally left proposals(128) * 4*21: [dx,dy,dw,dh](gt_boxes相对于rois) of 1 class
 (4): num of finally left proposals * 4*21: [1, 1, 1, 1] of 1 class
 (5): num of finally left proposals * 4*21: [true,true,true,true] of 1 class in 21; [false, false, false, false] in left of the classes
 """
